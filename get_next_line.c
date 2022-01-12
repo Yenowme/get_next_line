1c3cf2c70dd8b143447a901da266bf3c13d083ec
Yenowme <76904042+Yenowme@users.noreply.github.com>
2022-01-12 17:17:08 +0900

    norm v3 update

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yejeong <yejeong@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jeong-yena <jeong-yena@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 17:12:49 by yejeong           #+#    #+#             */
-/*   Updated: 2021/05/26 17:14:43 by yejeong          ###   ########.fr       */
+/*   Updated: 2022/01/12 17:15:24 by jeong-yena       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ static int	check_tmp(char **tmp, char **line)
 	char	*buff;
 
 	if (!*tmp)
-		if (!(*tmp = ft_strdup("")))
-			return (-1);
-	if ((end = ft_strchr(*tmp, '\n')))
+		*tmp = ft_strdup("");
+	end = ft_strchr(*tmp, '\n');
+	if (end)
 	{
 		*end = 0;
 		buff = *tmp;
 		*line = ft_strdup(*tmp);
-		if (!(*tmp = ft_strdup(end + 1)))
-			return (-1);
+		*tmp = ft_strdup(end + 1);
 		free(buff);
 		return (1);
 	}
@@ -38,52 +37,65 @@ static int	check_line(char **tmp, char **line)
 	char	*end;
 	char	*buff;
 
-	if ((end = ft_strchr(*line, '\n')))
+	end = ft_strchr(*line, '\n');
+	if (end)
 	{
 		*end = 0;
 		buff = *line;
 		if (*tmp)
-			if (!(*line = ft_strjoin(*tmp, *line)))
-				return (-1);
+			*line = ft_strjoin(*tmp, *line);
 		*tmp = ft_strdup(end + 1);
 		free(buff);
 		return (1);
 	}
 	else if (!*tmp)
-	{
-		if (!(*tmp = ft_strdup(*line)))
-			return (-1);
-	}
-	else if (!(*tmp = ft_strjoin(*tmp, *line)))
-		return (-1);
+		*tmp = ft_strdup(*line);
+	else
+		*tmp = ft_strjoin(*tmp, *line);
 	return (0);
 }
 
-int			get_next_line(int fd, char **line)
+int	read_line(int fd, char **line, char **tmp)
 {
 	int			rd_size;
-	static char	*tmp[OPEN_MAX];
 	int			rt;
 
-	if (BUFFER_SIZE <= 0 || !line || fd < 0 || fd > OPEN_MAX
-	|| read(fd, *line, 0) == -1)
-		return (-1);
-	if ((rt = check_tmp(&tmp[fd], line)))
-		return (rt);
-	if (!(*line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-		return (-1);
-	if ((rd_size = read(fd, *line, BUFFER_SIZE)) == -1)
+	rt = 1;
+	rd_size = read(fd, *line, BUFFER_SIZE);
+	if (rd_size == -1)
 		return (-1);
 	while (rd_size > 0)
 	{
 		(*line)[rd_size] = 0;
-		if ((rt = check_line(&tmp[fd], line)))
+		rt = check_line(tmp, line);
+		if (rt)
 			return (rt);
-		if ((rd_size = read(fd, *line, BUFFER_SIZE)) == -1)
+		rd_size = read(fd, *line, BUFFER_SIZE);
+		if (rd_size == -1)
 			return (-1);
 	}
+	if (!rt)
+		return (rt);
 	free(*line);
 	*line = tmp[fd];
 	tmp[fd] = 0;
 	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*tmp[OPEN_MAX];
+	int			rt;
+
+	if (BUFFER_SIZE <= 0 || !line || fd < 0 || fd > OPEN_MAX
+		|| read(fd, *line, 0) == -1)
+		return (-1);
+	rt = check_tmp(&tmp[fd], line);
+	if ((rt))
+		return (rt);
+	*line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!*line)
+		return (-1);
+	rt = read_line(fd, line, &tmp[fd]);
+	return (rt);
 }
